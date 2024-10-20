10de3f7518446b7ea3d493eecd5f15f33236493c
Kristian Kalweit <kallelix@gmail.com>
2024-10-20 21:45:29 +0200

    working webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <Arduino.h>
+#include <WebServer.h>
+//#include <AsyncJson.h>
+#include <ArduinoJson.h>
+#include <WiFiManager.h>
 #include "pinout.h"
+
+// Web server running on port 80
+WebServer server(80);
+JsonDocument jsonDocument;
+char buffer[250];
 
 // The max duty cycle value based on PWM resolution (will be 255 if resolution is 8 bits)
 const int MAX_DUTY_CYCLE = (int)(pow(2, PWM_RESOLUTION) - 1);
@@ -34,7 +43,7 @@ typedef struct
 } Scenario;
 
 const int DEFAULT_RAMP_DELAY_MS = 20; // delay between fade increments
-const int DEFAULT_CYCLE_DELAY_MS = 75000;
+const int DEFAULT_CYCLE_DELAY_MS = 15000;
 
 Scenario off = {DEFAULT_RAMP_DELAY_MS, 1, 0, DEFAULT_CYCLE_DELAY_MS};
 Scenario highest = {DEFAULT_RAMP_DELAY_MS, 1, 100, DEFAULT_CYCLE_DELAY_MS};
@@ -44,46 +53,97 @@ Scenario low = {DEFAULT_RAMP_DELAY_MS, 1, 30, DEFAULT_CYCLE_DELAY_MS};
 TaskHandle_t Task_living_room;
 Scenario Scenario_living_room;
 
+void TaskFanCycle(void *parameter);
+void setSpeed(pwm_channel_pair pair, int relativspeed);
+void getFan();
+void handlePost();
+
+void setup_routing() {     
+  //server.on("/temperature", getTemperature);     
+  //server.on("/pressure", getPressure);     
+  //server.on("/humidity", getHumidity);     
+  server.on("/fan", getFan);     
+  server.on("/set", HTTP_POST, handlePost);    
+          
+  server.begin();    
+}
+
+void create_json(const char *tag, const float value, const char *unit) {  
+  jsonDocument.clear();
+  jsonDocument["type"] = tag;
+  jsonDocument["value"] = value;
+  jsonDocument["unit"] = unit;
+  serializeJson(jsonDocument, buffer);  
+}
+
+void add_json_object(const char *tag, const float value, const char *unit) {
+  JsonObject obj = jsonDocument.add<JsonObject>();
+  obj["type"] = tag;
+  obj["value"] = value;
+  obj["unit"] = unit; 
+}
+
+void getFan() {
+  Serial.println("Get Fan Data");
+  jsonDocument.clear();
+  add_json_object("power", Scenario_living_room.power, "%");
+  add_json_object("cycle", Scenario_living_room.cycle_time_ms, "ms");
+  serializeJson(jsonDocument, buffer);
+  server.send(200, "application/json", buffer);
+}
+
+void handlePost() {
+  if (server.hasArg("plain") == false) {
+  }
+  String body = server.arg("plain");
+  deserializeJson(jsonDocument, body);
+
+  int power = jsonDocument["power"];
+  int cycle = jsonDocument["cycle"];
+  
+  Scenario_living_room.power = power;
+  Scenario_living_room.cycle_time_ms = cycle;
+
+  server.send(200, "application/json", "{}");
+}
+
 void setup()
 {
 
-  // Serial.begin(9600);
+  Serial.begin(9600);
   Serial.println("Starting...");
+  WiFiManager wm;
+  wm.autoConnect("AutoConnectAP","password");
 
   // Sets up a channel (0-15), a PWM duty cycle frequency, and a PWM resolution (1 - 16 bits)
   // ledcSetup(uint8_t channel, double freq, uint8_t resolution_bits);
   ledcSetup(living_room.channel_1, PWM_FREQ, PWM_RESOLUTION);
   ledcSetup(living_room.channel_2, PWM_FREQ, PWM_RESOLUTION);
-  ledcSetup(rest_rooms.channel_1, PWM_FREQ, PWM_RESOLUTION);
-  ledcSetup(rest_rooms.channel_2, PWM_FREQ, PWM_RESOLUTION);
+  //ledcSetup(rest_rooms.channel_1, PWM_FREQ, PWM_RESOLUTION);
+  //ledcSetup(rest_rooms.channel_2, PWM_FREQ, PWM_RESOLUTION);
 
   // ledcAttachPin(uint8_t pin, uint8_t channel);
-  ledcAttachPin(LED1_OUTPUT_PIN, living_room.channel_1);
-  ledcAttachPin(LED2_OUTPUT_PIN, living_room.channel_2);
+  //ledcAttachPin(LED1_OUTPUT_PIN, living_room.channel_1);
+  //ledcAttachPin(LED2_OUTPUT_PIN, living_room.channel_2);
   ledcAttachPin(FAN1_LIVING_ROOM_OUTPUT_PIN, living_room.channel_1);
   ledcAttachPin(FAN2_LIVING_ROOM_OUTPUT_PIN, living_room.channel_2);
 
   // init living room
-  Scenario_living_room = low;
+  Scenario_living_room = highest;
   Scenario_living_room.pair = living_room;
 
-  xTaskCreatePinnedToCore(
-      TaskFanCycle,                  /* Task function. */
-      "TaskCycleLivingRoom",         /* name of task. */
-      10000,                         /* Stack size of task */
-      (void *)&Scenario_living_room, /* parameter of the task */
-      1,                             /* priority of the task */
-      &Task_living_room,             /* Task handle to keep track of created task */
-      0);                            /* pin task to core 0 */
+  xTaskCreate(TaskFanCycle, "CycleLivingRoom", 10000, (void *)&Scenario_living_room, 1, &Task_living_room);
   delay(500);
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  setup_routing();
 }
 
 void TaskFanCycle(void *parameter)
 {
-
   Scenario *data = (Scenario *)parameter;
-  // Serial.print("Task1 running on core ");
-  // Serial.println(xPortGetCoreID());
+  Serial.print("Task FanCycle running on core ");
+  Serial.println(xPortGetCoreID());
   for (;;)
   {
     for (int fade = 0; fade <= data->power; fade++)
@@ -108,10 +168,11 @@ void TaskFanCycle(void *parameter)
       delay(data->ramp_delay_ms);
     }
   }
+  server.begin();
 }
 
 // set relativ speed: -100 to 100
-void setSpeed(pwm_channel_pair pair, int relativspeed = 0)
+void setSpeed(pwm_channel_pair pair, int relativspeed)
 {
   int dutyCycle = int(float(relativspeed + 100) / 200.0 * MAX_DUTY_CYCLE);
   int dutyCycleReverse = int(float(-1 * relativspeed + 100) / 200.0 * MAX_DUTY_CYCLE);
@@ -120,13 +181,23 @@ void setSpeed(pwm_channel_pair pair, int relativspeed = 0)
   ledcWrite(pair.channel_2, dutyCycleReverse);
   Serial.print("Relativ: ");
   Serial.print(relativspeed);
-  Serial.print(" duty1: ");
+  Serial.print("Max Duty: ");
+  Serial.print(MAX_DUTY_CYCLE);
+  Serial.print(" set pin1: ");
+  Serial.print(pair.channel_1);
+  Serial.print(" set duty1: ");
   Serial.print(dutyCycle);
+  Serial.print(" read duty1: ");
+  Serial.print(ledcRead(pair.channel_1));
+  Serial.print(" set pin2: ");
+  Serial.print(pair.channel_2);
   Serial.print(" duty2: ");
-  Serial.println(dutyCycleReverse);
-}
+  Serial.print(dutyCycleReverse);
+  Serial.print(" read duty2: ");
+  Serial.println(ledcRead(pair.channel_2));
+  }
 
 void loop()
 {
-
+  server.handleClient(); 
 }
--- src/pinout.h
+++ src/pinout.h
@@ -1,11 +1,11 @@
 // Papst Product Data Sheet 4412 FGMPR: https://www.farnell.com/datasheets/2634798.pdf
 // Lüfter mit Drehrichtungsumkehr. 0% - 45% PWM über Stege blasend, 55% - 100% über Stege saugend.
 
-const int PWM_FREQ = 5000;     // Official ESP32 example uses 5,000Hz as default
+const int PWM_FREQ = 2000;     // Official ESP32 example uses 5,000Hz as default
 const int PWM_RESOLUTION = 8; // resolution: 8 bits, 0-255
 
 // pinouts
 const int LED1_OUTPUT_PIN = 16;
-const int LED2_OUTPUT_PIN = 18;
-const int FAN1_LIVING_ROOM_OUTPUT_PIN = 17;
+const int LED2_OUTPUT_PIN = 17;
+const int FAN1_LIVING_ROOM_OUTPUT_PIN = 18;
 const int FAN2_LIVING_ROOM_OUTPUT_PIN = 19;
